6dd0b4214f97e7cedb9b131134255562800316f3
pashokkok <62620697+pashokkok@users.noreply.github.com>
2021-04-02 19:00:30 +0300

    Add files via upload

---
+++ kk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+echo "--------------------"
+echo "Кто ты ?
+echo "--------------------"
+echo "1.Павел
+echo "2Максим
+echo "
+echo "
+echo "
+echo "
+read numb
+if [ $numb = "1" ]
+then
+	echo "Здраствуйте Повелитель"
+if [ $numb = "2" ]
+then
+    echo "Приветствуем лоха!"
+		else
+			echo "Некорректный ввод"
+		fi
+	fi
+fi
