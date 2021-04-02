4dc461d189c2a48835873b03d033299c73ef5346
pashokkok <62620697+pashokkok@users.noreply.github.com>
2021-04-02 23:29:17 +0300

    Add files via upload

--- 1.0.py
+++ 1.0.py
@@ -1,24 +1,31 @@
+from colorama import init 
+from colorama import Fore, Back, Style
 
+# use Colorama to make Termcolor work on Windows too
+init()
 
-print(colored("█▄▀ ▄▀█ █░░ █▄▀ █░█ █░░ ▄▀█ ▀█▀ █▀█ █▀█", 'red'))
-print(colored("█░█ █▀█ █▄▄ █░█ █▄█ █▄▄ █▀█ ░█░ █▄█ █▀▄", 'red'))
+print( Back.RED )
 
+print("█▄▀ ▄▀█ █░░ █▄▀ █░█ █░░ ▄▀█ ▀█▀ █▀█ █▀█")
+print("█░█ █▀█ █▄▄ █░█ █▄█ █▄▄ █▀█ ░█░ █▄█ █▀▄")
 
-name = input ("nickname:")
-password = input ("password:")
+print ( Back.BLACK )
+print ( Fore.RED )
+name = input ("----nickname----:")
+password = input ("----password----:")
 
-print ("hello " + name + ", good morning =D")
+print ("----hello " + name + ", good morning =D----")
  
 what = input ("что делаем ? (+,-): " )
 
-a = float( input("пожалуйста введите цифру: ") )
-b = float( input("пожалуйста введите цифру: ") )
+a = float( input("----пожалуйста введите цифру----: ") )
+b = float( input("----пожалуйста введите цифру----: ") )
 
 if what == "+":
 	c = a + b
-	print("ваш результат: " + str(c))
+	print("----ваш результат: " + str(c) + "----")
 elif what == "-":
 	c = a - b
-	print("ваш результат: " + str(c))
+	print("----ваш результат: " + str(c) + "----")
 else:
-	print("ты дебил")
+	print("----ты дебил----")
